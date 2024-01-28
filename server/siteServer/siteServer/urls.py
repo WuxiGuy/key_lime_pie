6c98462ee3bb6405adff35d363308951de12bd7a
yuhaoh <hua.yuha@northeastern.edu>
2024-01-28 04:00:05 -0500

    build up backend with Django, and connect extension with server

--- server/siteServer/siteServer/urls.py
+++ server/siteServer/siteServer/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("lookAtData/", include("lookAtData.urls")),
+    path("", include("lookAtData.urls")),
     path('admin/', admin.site.urls),
 ]
